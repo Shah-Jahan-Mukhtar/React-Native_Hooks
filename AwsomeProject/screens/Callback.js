e193ca0f998254b661717fe9b6ede848423c5d5e
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-08 14:14:48 -0800

    useState ,useCallback, useEffect

--- AwsomeProject/screens/Callback.js
+++ AwsomeProject/screens/Callback.js
@@ -1,19 +1,30 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 const Callback = () => {
-  const [count, setCount] = useState({});
+  const [count, setCount] = useState(0);
+  const [Decrement, setDecrement] = useState(0);
 
   const handleIncrement = useCallback(() => {
-    setCount({ name: "Shahjahan", name2: "Abubakar" });
+    setCount(count + 1);
   }, [count]);
+  const handleDecrement = useCallback(() => {
+    setDecrement(Decrement - 1);
+  }, [Decrement]);
+
+  useEffect(() => {
+    handleDecrement();
+  }, [Decrement]);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignContent: "center" }}>
-      <Text>Count:{count.name}</Text>
-      <Text>Count:{count.name2}</Text>
+      <Text>Count:{count}</Text>
+      <Text>Count:{Decrement}</Text>
 
       <TouchableOpacity onPress={handleIncrement}>
-        <Text>Click to update</Text>
+        <Text>Click to Increment</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handleDecrement}>
+        <Text>Click to Decrement</Text>
       </TouchableOpacity>
     </View>
   );
