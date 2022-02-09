542789b06f10a58318cec67653a506c027b36a1d
Shah-Jahan-Mukhtar <shahjahanmukhtar62@gmail.com>
2022-02-09 13:53:17 -0800

    Hooks with Refresh control

--- AwsomeProject/screens/Callback.js
+++ AwsomeProject/screens/Callback.js
@@ -1,9 +1,17 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  RefreshControl,
+  ScrollView,
+  StyleSheet,
+} from "react-native";
 import React, { useState, useCallback, useEffect } from "react";
 
 const Callback = () => {
   const [count, setCount] = useState(0);
   const [Decrement, setDecrement] = useState(0);
+  const [Refersh, setRefresh] = useState(false);
 
   const handleIncrement = useCallback(() => {
     setCount(count + 1);
@@ -19,23 +27,55 @@ const Callback = () => {
     alert("Bari jaldi a tnu");
   }, [Decrement]);
 
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Count:{count}</Text>
-      <Text>Count:{Decrement}</Text>
+  const handleRefresh = useCallback(() => {
+    setRefresh(true);
+    setTimeout(() => {
+      setRefresh(false);
+    }, 1000);
+  }, []);
 
-      <TouchableOpacity onPress={handleIncrement}>
-        <Text style={{ backgroundColor: "cyan", marginTop: 20, fontSize: 40 }}>
-          Check Your habits
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleDecrement}>
-        <Text style={{ backgroundColor: "lime", marginTop: 20, fontSize: 40 }}>
-          Check Your Wife Name
-        </Text>
-      </TouchableOpacity>
-    </View>
+  return (
+    <ScrollView
+      style={styles.scrollView}
+      refreshControl={
+        <RefreshControl refreshing={Refersh} onRefresh={handleRefresh} />
+      }
+    >
+      <View
+        style={[{ flex: 1, justifyContent: "center", alignItems: "center" }]}
+      >
+        <Text>Count:{count}</Text>
+        <Text>Count:{Decrement}</Text>
+
+        <TouchableOpacity onPress={handleIncrement}>
+          <Text
+            style={{ backgroundColor: "cyan", marginTop: 20, fontSize: 40 }}
+          >
+            Check Your habits
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleDecrement}>
+          <Text
+            style={{ backgroundColor: "lime", marginTop: 20, fontSize: 40 }}
+          >
+            Check Your Wife Name
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: "pink",
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+});
 
 export default Callback;
